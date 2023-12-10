39148b339f6a414bb05f7e8e8020f71e22dde61b
Chubsster <evgenshama37@gmail.com>
2023-12-11 00:28:27 +0500

    Внесенные изменения.

--- src/main/java/ru/job4j/ru/job4j/Info.java
+++ src/main/java/ru/job4j/ru/job4j/Info.java
@@ -1,7 +1,9 @@
 package ru.job4j.ru.job4j;
 
 public class Info {
-    public static void main(String[] args){
-        System.out.println("10.12.23");
+    public static void main(String[] args) {
+            System.out.println("10.12.23");
     }
 }
+
+
