69ddb0e1c0b6643c39c7d04bf511938ee32ca535
Chubsster <evgenshama37@gmail.com>
2023-12-11 12:47:52 +0500

    Тип данных

---
+++ src/main/java/ru/job4j/Datatype.java
@@ -0,0 +1,11 @@
+package ru.job4j;
+
+public class Datatype {
+    public static void main(String[] args) {
+        int age = 18;
+        double lenght = 78.0332;
+        boolean baby = true;
+        System.out.println("age, lenght, baby");
+
+    }
+}
