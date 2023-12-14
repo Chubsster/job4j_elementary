c20ad1b7715319c3d308ddbf4d31f2c3b6f1215a
Chubsster <evgenshama37@gmail.com>
2023-12-15 00:05:11 +0500

    Модульные тесты. [#159435]

---
+++ src/test/java/ru/job4j/calculator/converter/ConverterTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.calculator.converter;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class ConverterTest {
+
+    @Test
+    void whenConvert140RblThen2Euro() {
+        float input = 140;
+        float expected = 2;
+        float output = Converter.rubleToEuro(input);
+        float value = 0.0001f;
+        Assertions.assertThat(output).isEqualTo(expected, withPrecision(value));
+    }
+
+    @Test
+    void whenConvert180RblThen3Dollar() {
+        float input = 180;
+        float expected = 3;
+        float output = Converter.rubleToDollar(input);
+        float value = 0.0001f;
+        Assertions.assertThat(output).isEqualTo(expected, withPrecision(value));
+    }
+
+    @Test
+    void whenConvert200RblThen3dot3333Evro() {
+        float input = 200;
+        float expected = 3.3333f;
+        float output = Converter.rubleToEuro(input);
+        float value = 0.0001f;
+        Assertions.assertThat(output).isEqualTo(expected, withPrecision(value));
+    }
+}
