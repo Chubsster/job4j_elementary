a9c40a210f8db8634751ff8292886c2ec622d0db
Chubsster <evgenshama37@gmail.com>
2023-12-16 17:40:28 +0500

    Входные данные в тесте [#297750 #466840]

--- src/main/java/ru/job4j/condition/X2Test.java
+++ src/main/java/ru/job4j/condition/X2Test.java
@@ -7,7 +7,7 @@ class X2Test {
 
     @Test
 
-    void whenA10B0C0X2Then() {
+    void whenA10B0C0X2Then40() {
         int a = 10;
         int b = 0;
         int c = 0;
@@ -15,26 +15,41 @@ class X2Test {
         int expected = 40;
         int result = X2.calc(a, b, c, x);
         assertThat(result).isEqualTo(expected);
-        a = 1;
-        b = 1;
-        c = 1;
-        x = 1;
-        expected = 3;
-        result = X2.calc(a, b, c, x);
+    }
+
+    @Test
+
+    void whenA1B1C1X1Then3() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 3;
+        int result = X2.calc(a, b, c, x);
         assertThat(result).isEqualTo(expected);
-        a = 1;
-        b = 1;
-        c = 0;
-        x = 1;
-        expected = 2;
-        result = X2.calc(a, b, c, x);
+    }
+
+    @Test
+
+    void whenA0B1C1X1Then2() {
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 2;
+        int result = X2.calc(a, b, c, x);
         assertThat(result).isEqualTo(expected);
-        a = 1;
-        b = 1;
-        c = 1;
-        x = 0;
-        expected = 1;
-        result = X2.calc(a, b, c, x);
+    }
+
+    @Test
+
+    void whenA1B1C1X0Then1() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 0;
+        int expected = 1;
+        int result = X2.calc(a, b, c, x);
         assertThat(result).isEqualTo(expected);
     }
 }
