c5805235b8bdf35807288980995403a96c5df231
Chubsster <evgenshama37@gmail.com>
2023-12-20 14:04:16 +0500

    3.Тесты для определения расстояния между точками. [#159459 #466884]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -1,50 +1,50 @@
 package ru.job4j.condition;
 
-import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.*;
-
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.Assert.*;
 public class PointTest {
     @Test
-    void when00To20then2() {
+    public void when00To20then2() {
         double expected = 2;
         int x1 = 0;
         int y1 = 0;
         int x2 = 2;
         int y2 = 0;
         double output = Point.distance(x1, y1, x2, y2);
-        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+        Assert.assertEquals(expected, output, 0.01);
     }
 
     @Test
-    void when22to42then2() {
+    public void when22to42then2() {
         double expected = 2;
         int x1 = 2;
         int y1 = 4;
         int x2 = 2;
         int y2 = 2;
         double output = Point.distance(x1, y1, x2, y2);
-        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+        Assert.assertEquals(expected, output, 0.01);
     }
 
     @Test
-    void whenMinus32toMinus62then9dot43() {
+    public void whenMinus32toMinus62then9dot43() {
         double expected = 9.43;
         int x1 = -3;
         int y1 = -6;
         int x2 = 2;
         int y2 = 2;
         double output = Point.distance(x1, y1, x2, y2);
-        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+        Assert.assertEquals(expected, output, 0.01);
     }
 
     @Test
-    void when5Minus1to64then12() {
-        double expected = 12;
+    public void when5Minus1to64then6dot32() {
+        double expected = 6.32;
         int x1 = 5;
         int y1 = 6;
         int x2 = -1;
         int y2 = 4;
         double output = Point.distance(x1, y1, x2, y2);
-        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+        Assert.assertEquals(expected, output, 0.01);
     }
 }
