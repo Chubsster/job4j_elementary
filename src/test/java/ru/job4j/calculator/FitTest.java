888c1f210a4c33a61afdd099f9353379f75a86fa
Chubsster <evgenshama37@gmail.com>
2023-12-20 21:30:17 +0500

    2. Тесты для задачи "идеальный вес" [#159458 #466848]

--- src/test/java/ru/job4j/calculator/FitTest.java
+++ src/test/java/ru/job4j/calculator/FitTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.calculator;
 
 import org.junit.jupiter.api.Test;
+import ru.job4j.calculator.converter.Fit;
 import static org.assertj.core.api.Assertions.*;
 
 public class FitTest {
