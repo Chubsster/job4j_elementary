6cbe0eba38f9b13142b0d5cdef5ab07428d4af9f
Chubsster <evgenshama37@gmail.com>
2023-12-20 13:15:25 +0500

    0. Операции сравнения [#235631 #467201]

--- src/main/java/ru/job4j/condition/AlertDivByZero.java
+++ src/main/java/ru/job4j/condition/AlertDivByZero.java
@@ -2,11 +2,11 @@ package ru.job4j.condition;
 
 public class AlertDivByZero {
     public static void checkNumber(int number) {
-        if (number == 0); {
+        if (number == 0) {
             System.out.println("Could not div by 0.");
         }
-        if (number < 0); {
-            System.out.println("This is negativ number");
+        if (number < 0) {
+            System.out.println("This is negativ number.");
         }
     }
 
