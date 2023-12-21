b15e49f193c1beecbbb53f0b04cc716b7a4e5f5c
Chubsster <evgenshama37@gmail.com>
2023-12-21 17:26:09 +0500

    4.1.2. Глупый бот [#31323]

--- src/main/java/ru/job4j/condition/DummyBot.java
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -3,18 +3,18 @@ package ru.job4j.condition;
 public class DummyBot {
     public static String answer(String question) {
         String result = "I don't know. Please, ask another question.";
-        if ("Hi, Bot".equals(question)) {
-        result = "Hi, SmartAss";
-        } else if ("Bye".equals(question)) {
-            result = "See yuo later";
+        if ("Hi, Bot.".equals(question)) {
+        result = "Hi, SmartAss.";
+        } else if ("Bye.".equals(question)) {
+            result = "See you later.";
         }
         return result;
 }
 
     public static void main(String[] args) {
-    String result = answer("Hi, Bot");
+    String result = answer("Hi, Bot.");
     System.out.println(result);
-    result = answer("Bye");
+    result = answer("Bye.");
     System.out.println(result);
     }
 }
