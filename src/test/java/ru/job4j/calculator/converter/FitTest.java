a7bf1f094702076b5dd3557f84fe8e1811f934c3
Chubsster <evgenshama37@gmail.com>
2023-12-21 10:47:54 +0500

    2. Тесты для задачи "идеальный вес" [#159458 #466848]

--- src/test/java/ru/job4j/calculator/converter/FitTest.java
+++ src/test/java/ru/job4j/calculator/converter/FitTest.java
@@ -6,7 +6,7 @@ import static org.assertj.core.api.Assertions.*;
 
 public class FitTest {
     @Test
-    void whenMan180then92() {
+    public void whenMan180then92() {
         short input = 180;
         double expected = 92;
         double output = Fit.manWeight(input);
@@ -14,7 +14,7 @@ public class FitTest {
     }
 
     @Test
-    void whenWoman170then69() {
+    public void whenWoman170then69() {
         short input = 170;
         double expected = 69;
         double output = Fit.womanWeight(input);
