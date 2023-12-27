2a936cc4d6622b53a886f24601016394a5b02452
Chubsster <evgenshama37@gmail.com>
2023-12-27 11:47:32 +0500

    4.2. Заголовок в файле java [#159036 #465734]

--- src/main/java/ru/job4j/ru/job4j/Info.java
+++ src/main/java/ru/job4j/ru/job4j/Info.java
@@ -2,7 +2,7 @@ package ru.job4j.ru.job4j;
 
 public class Info {
     public static void main(String[] args) {
-            System.out.println("10.12.23");
+        System.out.println("10.12.23");
     }
 }
 
