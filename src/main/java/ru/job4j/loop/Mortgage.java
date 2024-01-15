63f3b51dd48d870afb020c40de7694dee8caa2fe
Chubsster <evgenshama37@gmail.com>
2024-01-15 10:27:17 +0500

    5.7. Ипотека [#156315 #473351]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,10 +3,9 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
-        double credit = amount;
-        while (credit > 0) {
+        while (amount > 0) {
         year++;
-        credit = credit + (credit * percent / 100) - salary * 12;
+        amount = amount + (amount * percent / 100) - salary;
     }
     return year;
 }
