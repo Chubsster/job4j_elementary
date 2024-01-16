061ca9d4ad994f748dfefc0db913e0fcc6708d29
Chubsster <evgenshama37@gmail.com>
2024-01-16 12:09:05 +0500

    6.4.3. Слово заканчивается на ... [#156317]

---
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -0,0 +1,14 @@
+package ru.job4j.array;
+
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] postfix) {
+        boolean result = true;
+        int index = word.length - 1;
+        for (int i = postfix.length - 1; i > 0; i--) {
+            if (word[index] != postfix[i]) {
+                result = false;
+            }
+        }
+        return result;
+    }
+}
