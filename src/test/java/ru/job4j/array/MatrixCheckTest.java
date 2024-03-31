da387ca86832fb659b8533cc613fbf1d85513fb5
Chubsster <evgenshama37@gmail.com>
2024-03-31 15:52:01 +0500

    6.7.3. Массив из диагонали матрицы.  [#214128]

--- src/test/java/ru/job4j/array/MatrixCheckTest.java
+++ src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -2,8 +2,7 @@ package ru.job4j.array;
 
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
-import static ru.job4j.array.MatrixChek.monoHorizontal;
-import static ru.job4j.array.MatrixChek.monoVertical;
+import static ru.job4j.array.MatrixChek.*;
 
 class MatrixCheckTest {
     @Test
@@ -52,5 +51,40 @@ class MatrixCheckTest {
         int column = 2;
         boolean result = monoVertical(input, column);
         assertThat(result).isFalse();
+    }
+    @Test
+    public void whenDiagonalFullX() {
+        char[][] input = {
+                {'X', ' ', ' '},
+                {' ', 'X', ' '},
+                {' ', ' ', 'X'},
+        };
+        char[] result = extractDiagonal(input);
+        char[] expected = {'X', 'X', 'X'};
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    public void whenDiagonalFullOne() {
+        char[][] input = {
+                {'1', ' ', ' '},
+                {' ', '1', ' '},
+                {' ', ' ', '1'},
+        };
+        char[] result = extractDiagonal(input);
+        char[] expected = {'1', '1', '1'};
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    public void whenDiagonalMix() {
+        char[][] input = {
+                {'X', ' ', ' '},
+                {' ', 'Y', ' '},
+                {' ', ' ', 'Z'},
+        };
+        char[] result = extractDiagonal(input);
+        char[] expected = {'X', 'Y', 'Z'};
+        assertThat(result).containsExactly(expected);
     }
 }
